4b349bb6773e3712791b43300994c1eed5426314
clebim <matheuscleber1998@gmail.com>
2024-01-09 22:02:02 -0300

    add ci em pull request para develop

--- src/math_test.js
+++ src/math_test.js
@@ -2,8 +2,7 @@ import { test } from "node:test"
 import assert from "node:assert"
 import { soma } from "./math.js";
 
-test('spies on a function', () => {
-    const sum = soma(10, 15)
-
+test('test sum function', () => {
+    const sum = soma(15, 15)
     assert.equal(sum, 30)
 });
